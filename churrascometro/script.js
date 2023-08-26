56d218765aff132bd93040c6bad4b656034c593f
Gabriel372 <124525153+Gabriel372@users.noreply.github.com>
2023-08-26 15:15:35 -0300

    Update script.js

--- churrascometro/script.js
+++ churrascometro/script.js
@@ -1,4 +1,3 @@
-//CODIGO PROG BR
 //analize...
 let inputAdultos = document.getElementById('adultos')
 let inputCrianças = document.getElementById('crianças')
@@ -6,7 +5,6 @@ let inputDuraçao = document.getElementById('duraçao')
 let resultado = document.getElementById('resultado')//resposta ao html
 
 function calcular() {
-    console.log('calculando...')//simulando calculos js no console
 let adultos = inputAdultos.value//valor digitado no input
 let crianças = inputCrianças.value
 let duraçao = inputDuraçao.value
@@ -42,73 +40,3 @@ function bebidasPP(duraçao) {
         return 1000
     }
 }
-//========================
-// function calcular() {
-//     //input.value
-//     let an = document.getElementById('adultos')//0.4
-//     let cn = document.getElementById('crianças')//0.2
-//     let hn = document.getElementById('duraçao')
-//     // console.log(an.value , cn.value , hn.value )
-//     // let adultos = Number(an.value)
-//     // let crianças =  Number(cn.value)
-//     //div res
-//     let rescarne = document.getElementById('rescarne')
-//     // let rescerva = document.getElementById('rescerva')
-//     // let resrefri = document.getElementById('resrefri')
-//     //carne
-//     let carneadulto = 0.4
-//     let carnecriança = 0.2
-//     //cerveja
-//     let cervaadult = 4 //latas
-//     let cervacri = 0 //criança nao bebe
-//   //bebidas refri 2l
-//     let refriadult = 0.7 //ml
-//     let refricri = 1.3 //ml
-//     let refri2l = refriadult + refricri 
-//     // let pessoa = Number(an.value) ||
-// //1 adutlo e 1 criança= 1 garrf.2lt refri || 2 adutl == 1 garrf.2lt refri 
-// //a cada 2 pessoas == 1 garrf.2lt refri 
-//     if (an.value.length== 0 && cn.value.length== 0 && hn.value.length== 0) {
-//     // if (Number(an.value)==0 && Number(cn.value) && Number(hn.value)== 0) {
-//         rescarne.innerHTML = ``
-//         window.alert('Favor preencher os formulários')
-//         an.focus()
-//     }
-// else if (an.value.length> 0 && cn.value.length> 0 && hn.value.length> 0) {
-//  let adultos = an.value*carneadulto//0.4*adulto
-//  let crianças =  cn.value*carnecriança//0,2*criança
-// let carne = adultos+crianças
-// rescarne.innerHTML = `${carne.toFixed(2)} kg de carne`
-// // an.value = '' //zera valor digitado no input
-// let adultsbebecerva = Number(an.value)*cervaadult
-// let criansbebecerva = 0
-// rescarne.innerHTML += `<br><br>${adultsbebecerva} Latas de cerveja`
-// an.focus() //mantem cursor no input
-// }}
-//========================
-//refri
-// if () { 
-// }
-// let hora = Number(hn.value)
-// let refriacriaeadult = Number(an.value) + Number(cn.value) + hora
-// if (refriacriaeadult += 1) {
-//     rescarne.innerHTML += `<br><br>${refriacriaeadult} Garrafas de 2lt de Refrigerante` 
-// }
-// carne.splice(2);
-// adultos = 2
-// crianças = 2
-// alert(`estimativa de consumo ${carne.toFixed(2)} kg de carne`)//tofixed(2)reduz o nº de zero a direita
-//CALCULOS
-//adultos
-// let cerveja = 3.5
-//refrigerante = 2
-//crianças
-// let cervejacriança = 0
-//refrigerante = 1
-//duraçao
-//hora = 60 min
-//RESPOSTA HTML
-//pseudocodigo
-//receber
-//processar
-//mostrar resultados
